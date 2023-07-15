b50a6031301ae92a24f6ae5985c02ef483521015
Xevans <43121988+Xevans@users.noreply.github.com>
2023-07-14 21:36:25 -0400

    Home, welcome, account views set up

--- smartsplit/urls.py
+++ smartsplit/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import Welcome
+from .views import Welcome, Homepage, Account
 
 urlpatterns = [
-    path("", Welcome.as_view(), name="home") # when a user visits websitName.com/ they will be directed to the view, welcome
+    path("", Welcome.as_view(), name="welcome"), # when a user visits websitName.com/ they will be directed to the view, welcome
+    path("home/", Homepage.as_view(), name="home"),
+    path("account/", Account.as_view(), name="account")
 ]
--- smartsplit/views.py
+++ smartsplit/views.py
@@ -3,4 +3,10 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 class Welcome(TemplateView):
+    template_name = "welcome.html"
+
+class Homepage(TemplateView):
     template_name = "home.html"
+
+class Account(TemplateView):
+    template_name = "account.html"
