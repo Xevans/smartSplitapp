afb740d2d783857e6b631559a0629012ba41a0d3
Xevans <43121988+Xevans@users.noreply.github.com>
2023-07-16 11:59:28 -0400

    Sign up system built

--- user_accounts/views.py
+++ user_accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
 
 # Create your views here.
+
+class SignUpView(generic.CreateView): # Note to self, generic.CreateView is a generic-based-view
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login") # redirect to login when sign up is completed
+    template_name = "registration/signup.html"
