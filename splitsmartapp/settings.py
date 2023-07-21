64f25950fa422b13e0f34650ca916e7d40114677
xavier evans <evans.xavier@ymail.com>
2023-07-21 14:40:55 -0400

    custom login system tested.

--- splitsmartapp/settings.py
+++ splitsmartapp/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'smartsplit',
-    'user_accounts',
+    #'user_accounts',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "welcome"
+
+AUTH_USER_MODEL = "accounts.CustomUser"
