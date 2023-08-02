6d9821e9fd6b87e847e0a41054d3d7b45dcd2abd
xavier evans <evans.xavier@ymail.com>
2023-08-02 09:33:45 -0400

    Updated models.py

--- smartsplit/models.py
+++ smartsplit/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # payment request model
@@ -14,3 +15,18 @@ class SentHistory(models.Model):
     message = models.CharField(max_length=100, default="")
     request_amount = models.FloatField(max_length=100, default = 0.00)
     recipient = models.CharField(max_length=100, default="")
+
+# Friend list Model
+class FriendList(models.Model):
+    foreign_key = models.ForeignKey('User', on_delete=models.CASCADE)
+    friend_username = models.CharField(max_length=100, default="")
+    friend_name = models.CharField(max_length=100, default="")
+
+
+# Friend request model
+class FriendRequests(models.Model):
+    sender = models.CharField(max_length=100, default="")
+    message = models.CharField(max_length=100, default="")
+    recipient = models.CharField(max_length=100, default="")
+    status = models.BooleanField(defaul=True)
+    
